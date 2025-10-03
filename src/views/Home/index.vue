74cac83c3621781915288966b48c4c5ca1d8c6fd
Husl <Husl@gmail.com>
2025-10-03 17:41:29 +0800

    面板组件封装与新鲜人气业务实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,12 +5,14 @@
     <HomeBanner />
   </div>
   <HomeNew />
+  <HomeHot/>
   <homeProduct />
 </template>
 <script setup>
 import HomeCategory from '@/views/Home/components/HomeCategory.vue'
 import HomeBanner from '@/views/Home/components/HomeBanner.vue'
 import HomeNew from '@/views/Home/components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
 import HomeProduct from '@/views/Home/components/HomeProduct.vue'
 </script>
 
