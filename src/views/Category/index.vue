18811d0092c9a3d375a7995b8299eb7876cdb274
Husl <Husl@gmail.com>
2025-10-06 16:32:36 +0800

    分类页表显示

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,7 +4,7 @@ import { getCategoryAPI } from '@/apis/category';
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import {getBannerAPI} from '@/apis/home'
-
+import GoodItem from '../Home/components/GoodItem.vue';
 // 获取数据
 const categoryData = ref({})
 const route = useRoute()
@@ -43,6 +43,25 @@ onMounted(() => getBanner())
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink :to="`/category/sub/${i.id}`">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodItem v-for="goods in item.goods" :goods="goods" :key="goods.id" />
+        </div>
       </div>
     </div>
   </div>
