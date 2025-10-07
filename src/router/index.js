48b33a807f47b0d6bb93da54aa4642d13085e0bd
Husl <Husl@gmail.com>
2025-10-07 14:21:07 +0800

    二级分类-整体认识

--- src/router/index.js
+++ src/router/index.js
@@ -3,28 +3,33 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // path和component对应关系的位置
   routes: [
     {
-      path:'/',
-      component:Layout,
-      children:[
+      path: '/',
+      component: Layout,
+      children: [
         {
-          path:'',
-          component:Home
+          path: '',
+          component: Home
         },
         {
-          path:'category/:id',
-          component:Category
-        }
+          path: 'category/:id',
+          component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
+        },
       ]
     },
     {
       path: '/login',
-      component:Login
+      component: Login
     }
   ],
 })
