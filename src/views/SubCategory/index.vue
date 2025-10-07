6b19a81fcf365ea65624ec3292a3837fe2f61b53
Husl <Husl@gmail.com>
2025-10-07 15:53:06 +0800

    二级分类-页面无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -4,7 +4,6 @@ import {getCategoryFilterAPI,getSubCategoryAPI} from '@/apis/category'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import GoodsItem from '../Home/components/GoodsItem.vue';
-import { get } from '@vueuse/core';
 // 获取面包屑导航数据
 const route = useRoute()
 const filterData = ref({})
@@ -22,8 +21,19 @@ const reqData = ref({
 })
 const getGoodsList = async () => {
   const res = await getSubCategoryAPI(reqData.value)
-  console.log(res);
   goodsList.value = res.result.items
+}
+// 加载更多
+const disabled = ref(false)
+const load = async () => {
+  // 获取下一页数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodsList.value = [...goodsList.value, ...res.result.items]
+  // 加载完毕,停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
 }
 onMounted(() => {
   getFilterData()
@@ -54,7 +64,7 @@ const tabChange =()=>{
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
          <GoodsItem v-for="goods in goodsList" :goods="goods" :key="goods.id"/>
       </div>
