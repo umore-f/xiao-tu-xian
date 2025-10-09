3b616a63947306dd0eacbff5287060e150206f73
Husl <Husl@gmail.com>
2025-10-09 17:29:25 +0800

    登录-401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
 import { useUserStore } from "@/stores/user";
+import {useRouter} from 'vue-router'
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -25,12 +26,21 @@ httpInstance.interceptors.request.use(config => {
 }, e => Promise.reject(e))
 // axios响应拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
+  const router = useRouter()
   // 这里通过拦截器的方式，来发送提示信息，
   // 这个response是拦截到响应码不是200了就会执行这个
   ElMessage({
     type: "warning",
     message: e.response.data.message,
   });
+  // 401token失效处理
+  // 1.清除本土用户数据
+  // 2.跳转到登录页
+  if (e.response.status === 401) {
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
