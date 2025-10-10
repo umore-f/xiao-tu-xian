5906c33cab293d0ba12584c6537b8dada16fa587
Husl <Husl@gmail.com>
2025-10-10 16:02:18 +0800

    本地购物车-头部购物车计算逻辑

--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -6,7 +6,7 @@ const cartStore = useCartStore()
 <template>
   <div class="cart">
     <a class="curr" href="javascript:;">
-      <i class="iconfont icon-cart"></i><em>{{ cartStore.cartList.length }}</em>
+      <i class="iconfont icon-cart"></i><em v-if="cartStore.cartList.length">{{ cartStore.cartList.length }}</em>
     </a>
     <div class="layer">
       <div class="list">
@@ -30,8 +30,8 @@ const cartStore = useCartStore()
       </div>
       <div class="foot">
         <div class="total">
-          <p>共 10 件商品</p>
-          <p>&yen; 100.00 </p>
+          <p>共 {{ cartStore.allCount }} 件商品</p>
+          <p>&yen; {{ cartStore.allPrice.toFixed(2) }} </p>
         </div>
         <el-button size="large" type="primary" >去购物车结算</el-button>
       </div>
