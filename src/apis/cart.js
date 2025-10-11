60fe2af4919d2903cbd61a35c90168d78c5dc82a
Husl <Husl@gmail.com>
2025-10-11 15:21:53 +0800

    接口购物车实现

---
+++ src/apis/cart.js
@@ -0,0 +1,18 @@
+import request from '@/utils/http'
+// 加入购物车
+export const insertCartAPI = ({ skuId, count }) => {
+  return request({
+    url: '/member/cart',
+    method: 'POST',
+    data: {
+      skuId,
+      count
+    }
+  })
+}
+// 获取购物车列表
+const findNewCartListAPI = () => {
+  return request({
+    url:'/member/cart'
+  })
+}
--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,22 +1,35 @@
 // 封装购物车模块
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
+import { useUserStore } from './user'
+import {insertCartAPI,findNewCartListAPI} from '@/apis/cart'
 
 export const useCartStore = defineStore('cart', () => {
+  const useStore = useUserStore()
+  const isLogin = computed(() => useStore.userInfo.token)
   // 1.定义state - cartList
   const cartList = ref([])
   // 2.定义action - addCart
-  const addCart = (goods) => {
-    // 添加购物车操作
-    // 添加过 - count + 1
-    // 没添加过 - 直接push
-    const item = cartList.value.find((item) => goods.skuId === item.skuId)
-    if (item) {
-      // 找到了
-      item.count++
+  const addCart = async (goods) => {
+    const {skuId,count} = goods
+    if (isLogin.value) {
+      // 登录之后的加入购车逻辑
+      await insertCartAPI({ skuId, count })
+      const res = await findNewCartListAPI()
+      cartList.value = res.result
+      // updateNewList()
     } else {
-      // 没找到
-      cartList.value.push(goods)
+      // 添加购物车操作
+      // 添加过 - count + 1
+      // 没添加过 - 直接push
+      const item = cartList.value.find((item) => goods.skuId === item.skuId)
+      if (item) {
+        // 找到了
+        item.count++
+      } else {
+        // 没找到
+        cartList.value.push(goods)
+      }
     }
   }
   // 删除购物车
@@ -31,9 +44,9 @@ export const useCartStore = defineStore('cart', () => {
     item.selected = selected
   }
   // 全选功能
-  const allCheck = (selected)=>{
+  const allCheck = (selected) => {
     // 把cartList中的每一项的selected都设置为当前的全选框状态
-    cartList.value.forEach((item)=>{item.selected = selected})
+    cartList.value.forEach((item) => { item.selected = selected })
   }
   // 计算属性
   // 1.总的数量 所有项的count之和
@@ -41,11 +54,11 @@ export const useCartStore = defineStore('cart', () => {
   // 2.总价 所有项的count*price之和
   const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
   // 3.已选择数量
-  const selectCount = computed(() => cartList.value.filter((item)=>item.selected).reduce((a, c) => a + c.count, 0))
+  const selectCount = computed(() => cartList.value.filter((item) => item.selected).reduce((a, c) => a + c.count, 0))
   // 4.已选择商品价钱合计
-  const selectCountPrice = computed(() => cartList.value.filter((item)=>item.selected).reduce((a, c) => a + c.count * c.price, 0))
+  const selectCountPrice = computed(() => cartList.value.filter((item) => item.selected).reduce((a, c) => a + c.count * c.price, 0))
   // 是否全选
-  const isAll = computed(() => cartList.value.every((item)=>item.selected))
+  const isAll = computed(() => cartList.value.every((item) => item.selected))
   return {
     cartList,
     addCart,
