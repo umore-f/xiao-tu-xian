194c16f58a92741a8a7d5353eb1e975567711bef
Husl <Husl@gmail.com>
2025-10-13 14:18:58 +0800

    结算-地址切换-打开弹窗

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -3,17 +3,21 @@
 import { getCheckoutInfoAPI } from '@/apis/checkout'
 // import {useCartStore} from '@/stores/cartStore'
 
-import {onMounted, ref} from 'vue'
+import { onMounted, ref } from 'vue'
 const checkInfo = ref({})  // 订单对象
 const curAddress = ref({})  // 地址对象
 // const cartStore = useCartStore()/
 const getCheckInfo = async () => {
   const res = await getCheckoutInfoAPI()
   checkInfo.value = res.result
-  const item = checkInfo.value.userAddresses.find(item=>item.isDefault === 0)
+  const item = checkInfo.value.userAddresses.find(item => item.isDefault === 0)
   curAddress.value = item
 }
-onMounted(()=>{getCheckInfo()})
+onMounted(() => { getCheckInfo() })
+
+
+// 控制弹窗
+let toggleFlag = ref(false)
 
 </script>
 
@@ -115,6 +119,23 @@ onMounted(()=>{getCheckInfo()})
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog v-model="toggleFlag" title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+        <ul>
+          <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary" @click="toggleFlag = false">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
