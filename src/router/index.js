5ac01240fe9fb30fc068f53b32108d9aab097c00
Husl <Husl@gmail.com>
2025-10-13 17:44:11 +0800

    会员中心-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,9 @@ import CarList from '@/views/CarList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
-
+import Member from '@/views/Member/index.vue'
+import MemberInfo from '@/views/Member/components/Userinfo.vue'
+import MemberOrder from '@/views/Member/components/UserOrder.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // path和component对应关系的位置
@@ -49,6 +51,20 @@ const router = createRouter({
         {
           path: 'paycallback',
           component: PayBack
+        },
+        {
+          path: 'member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: MemberInfo
+            },
+            {
+              path: 'order',
+              component: MemberOrder
+            }
+          ]
         }
       ]
     },
