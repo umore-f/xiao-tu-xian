c1445a4cb5830edbe370e631bc0f8753121c542c
Husl <Husl@gmail.com>
2025-11-02 15:42:13 +0800

    部署优化

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
 import Member from '@/views/Member/index.vue'
-import MemberInfo from '@/views/Member/components/Userinfo.vue'
+import MemberInfo from '@/views/Member/components/UserInfo.vue'
 import MemberOrder from '@/views/Member/components/UserOrder.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
